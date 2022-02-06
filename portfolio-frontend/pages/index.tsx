80199bfb07cef6ec86e9db77eaacec11401ba2f2
jianrong7 <60573059+jianrong7@users.noreply.github.com>
2022-02-06 21:42:28 +0800

    restyled about me section

--- portfolio-frontend/pages/index.tsx
+++ portfolio-frontend/pages/index.tsx
@@ -41,25 +41,23 @@ const Home: NextPage = () => {
             </a>
           </div>
         </div>
-        <section id="about">
-          <div className={styles.about}>
-            <div>
-              <h2>About me</h2>
-              <p>
-                I am a full-stack developer based in Singapore with a passion
-                for building digital services. I started my journey on web
-                development since 2020. My main focus is in building high
-                quality, performant and scalable code. I make sure to bring
-                consistency in UX and tooling across my projects.
-              </p>
-              <p>
-                I recently joined 99.co as a frontend engineer where my work
-                will consist of constructing beautiful UI components to help 99%
-                of people find their homes. When not online, I love working out
-                and reading about all kinds of things.
-              </p>
-              <Button>Resume</Button>
-            </div>
+        <section id="about" className={styles.about}>
+          <div>
+            <h2>About me</h2>
+            <p>
+              I am a full-stack developer based in Singapore with a passion for
+              building digital services. I started my journey on web development
+              since 2020. My main focus is in building high quality, performant
+              and scalable code. I make sure to bring consistency in UX and
+              tooling across my projects.
+            </p>
+            <p>
+              I recently joined 99.co as a frontend engineer where my work will
+              consist of constructing beautiful UI components to help 99% of
+              people find their homes. When not online, I love working out and
+              reading about all kinds of things.
+            </p>
+            <Button>Resume</Button>
           </div>
         </section>
         <section id="work">
