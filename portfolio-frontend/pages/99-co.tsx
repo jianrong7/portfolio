cd800b8277824f23b4034b296b5a601ae8f3bed3
jianrong7 <jianrong@99.co>
2022-02-11 18:42:34 +0800

    add 99co route

--- portfolio-frontend/pages/99-co.tsx
+++ portfolio-frontend/pages/99-co.tsx
@@ -1,18 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import About from "../components/home/About/About";
-import Intro from "../components/home/Intro/Intro";
-import Work from "../components/home/Work/Work";
-import Contact from "../components/home/Contact/Contact";
-
-import styles from "../styles/Home.module.css";
+import styles from "../styles/99.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Loh Jian Rong | Personal Portfolio</title>
+        <title>99.co | Work</title>
         <meta
           name="description"
           content="Loh Jian Rong is a software engineer currently based in Singapore working with React and Express."
@@ -21,10 +16,10 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Intro />
+        {/* <Intro />
         <About />
         <Work />
-        <Contact />
+        <Contact /> */}
       </main>
     </div>
   );
