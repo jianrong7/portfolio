d646030002dff18080d6da879a53e70c2372a1a1
jianrong7 <jianrong@99.co>
2022-02-22 21:38:08 +0800

    add google search console meta tag

--- portfolio-frontend/pages/_document.tsx
+++ portfolio-frontend/pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
         />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="google-site-verification"
+          content="42JqgY9JufdfzPi39TUgMEw4LcW6QhXvR27AOQxRerI"
+        />
       </Head>
       <body>
         <Main />
