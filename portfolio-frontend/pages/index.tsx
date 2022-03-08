89c07c265c7bde2ef651f1fc3c000e4eacc3561d
jianrong7 <jianrong@99.co>
2022-03-08 17:58:58 +0800

    add bye

--- portfolio-frontend/pages/index.tsx
+++ portfolio-frontend/pages/index.tsx
@@ -46,6 +46,7 @@ const Home: NextPage = () => {
       </Head>
       <NavBar />
       <main className={styles.main}>
+        wassup bye
         <Intro />
         <About />
         <Experience />
