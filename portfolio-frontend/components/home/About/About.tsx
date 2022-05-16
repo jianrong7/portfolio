a110a752b39fb0df199e1928d2a9a743fdc4f9d6
jianrong7 <60573059+jianrong7@users.noreply.github.com>
2022-05-16 21:12:07 +0800

    edits to about me section

--- portfolio-frontend/components/home/About/About.tsx
+++ portfolio-frontend/components/home/About/About.tsx
@@ -13,37 +13,33 @@ const About = () => {
   return (
     <section id="about" className={styles.about}>
       <div className={styles.aboutDiv}>
-        <div>
-          <Heading level="h2">About me</Heading>
-          <div className={styles.description}>
-            <p>
-              I am a full-stack developer based in Singapore with a passion for
-              building digital services. I started my journey on web development
-              since 2020. My main focus is in building high quality, performant
-              and scalable code. I make sure to bring consistency in UX and
-              tooling across my projects.
-            </p>
-            <p>
-              I recently joined 99.co as a frontend engineer where my work will
-              consist of constructing beautiful UI components to help 99% of
-              people find their homes. When not online, I love working out and
-              reading about all kinds of things.
-            </p>
-          </div>
-          <Link href="/resume.pdf" passHref>
-            <Button className={styles.resumeButton}>
-              <span>Resume.pdf</span>
-              <Image
-                src={
-                  theme === "light" ? "/download_light.png" : "/download.png"
-                }
-                width={24}
-                height={24}
-                alt="download"
-              />
-            </Button>
-          </Link>
+        <Heading level="h2">About me</Heading>
+        <div className={styles.description}>
+          <p>
+            I am a full-stack developer based in Singapore with a passion for
+            building digital services. I started my journey on web development
+            since 2020. My main focus is in building high quality, performant
+            and scalable code. I make sure to bring consistency in UX and
+            tooling across my projects.
+          </p>
+          <p>
+            I recently joined 99.co as a frontend engineer where my work will
+            consist of constructing beautiful UI components to help 99% of
+            people find their homes. When not online, I love working out and
+            reading about all kinds of things.
+          </p>
         </div>
+        <Link href="/resume.pdf" passHref>
+          <Button className={styles.resumeButton}>
+            <span>Resume.pdf</span>
+            <Image
+              src={theme === "light" ? "/download_light.png" : "/download.png"}
+              width={24}
+              height={24}
+              alt="download"
+            />
+          </Button>
+        </Link>
       </div>
     </section>
   );
