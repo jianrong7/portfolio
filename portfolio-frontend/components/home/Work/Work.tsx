2d12589c75ee0109d051b8944c168aee50e18238
jianrong7 <jianrong@99.co>
2022-07-08 22:03:19 +0800

    remove imports

--- portfolio-frontend/components/home/Work/Work.tsx
+++ portfolio-frontend/components/home/Work/Work.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import { useTheme } from "next-themes";
 
 import Heading from "../../shared/Heading/Heading";
-import CallToAction from "../../shared/CallToAction/CallToAction";
 import Project from "../../shared/Project/Project";
 import { driveSg, traceify, cryptoWatchlist } from "./workContent";
 
 import styles from "./Work.module.css";
 
 const Work = () => {
-  const { theme } = useTheme();
-
   return (
     <section id="work">
       <Heading level="h2">Featured Works</Heading>
